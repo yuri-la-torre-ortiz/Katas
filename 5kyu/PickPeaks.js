57fd02548f40713387607861e4d582337957b82d
Yuri La Torre Ortiz <yuri.la.torre.ortiz@protonmail.ch>
2020-09-01 16:40:07 +0200

    Pass all but one test

--- 5kyu/PickPeaks.js
+++ 5kyu/PickPeaks.js
@@ -19,27 +19,30 @@ For example: pickPeaks([1, 2, 2, 2, 1]) returns {pos: [1], peaks: [2]} (or equiv
 Have fun!
 */
 function pickPeaks(arr){
-  //  return {pos:[],peaks:[]}
-var pos = [];
-var peaks = [];
-var plateaux = [];
-arr.forEach((num, i) => {
-  if (arr[i-1] < num && arr[i+1] < num) {
-    pos.push(i);
-    peaks.push(num);
-  }
-  if (arr[i+1] === num || arr[i-1] === num){
-    if(plateaux.length !== 0) {
-      if(num === plateaux[plateaux.length-1].num) {
-      plateaux[plateaux.length-1].i.push(i);
-    } else { plateaux.push({num, i: [i]});
-    }}
-    else { plateaux.push({num, i: [i]});
+    //  return {pos:[],peaks:[]}
+  var pos = [];
+  var peaks = [];
+  var plateaux = [];
+  arr.forEach((num, i) => {
+    if (arr[i-1] < num && arr[i+1] < num) {
+      pos.push(i);
+      peaks.push(num);
     }
-   } 
-  });
-  console.log(plateaux)
-return { peaks, pos}
+    if (arr[i+1] === num || arr[i-1] === num){
+      if(plateaux.length !== 0) {
+        if(num === plateaux[plateaux.length-1].num) {
+        plateaux[plateaux.length-1].i.push(i);
+      } else { plateaux.push({num, i: [i]});
+      }}
+      else { plateaux.push({num, i: [i]});
+      }
+      if(plateaux[plateaux.length-1].num > arr[Math.min(...plateaux[plateaux.length-1].i) -1] && plateaux[plateaux.length-1].num > arr[Math.max(...plateaux[plateaux.length-1].i) +1]) {
+       pos.push(Math.min(...plateaux[plateaux.length-1].i));
+      peaks.push(plateaux[plateaux.length-1].num);
+      }
+     } 
+    });
+  return { peaks, pos}
 }
 pickPeaks([3,2,3,6,4,1,2,3,2,1,2,2,2,1]);
 pickPeaks([1,2,5,4,3,2,3,6,4,1,2,3,3,4,5,3,2,1,2,3,5,5,4,3])
