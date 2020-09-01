fae8aa0a9d9d10f272db37e363be1ac50a5c9a08
Yuri La Torre Ortiz <yuri.la.torre.ortiz@protonmail.ch>
2020-09-01 15:57:09 +0200

    Group plateaux

--- 5kyu/PickPeaks.js
+++ 5kyu/PickPeaks.js
@@ -19,22 +19,27 @@ For example: pickPeaks([1, 2, 2, 2, 1]) returns {pos: [1], peaks: [2]} (or equiv
 Have fun!
 */
 function pickPeaks(arr){
-    //  return {pos:[],peaks:[]}
-  var pos = [];
-  var peaks = [];
-  var plateau = [];
-  arr.forEach((num, i) => {
-    if (arr[i-1] < num && arr[i+1] < num) {
-      pos.push(i);
-      peaks.push(num);
+  //  return {pos:[],peaks:[]}
+var pos = [];
+var peaks = [];
+var plateaux = [];
+arr.forEach((num, i) => {
+  if (arr[i-1] < num && arr[i+1] < num) {
+    pos.push(i);
+    peaks.push(num);
+  }
+  if (arr[i+1] === num || arr[i-1] === num){
+    if(plateaux.length !== 0) {
+      if(num === plateaux[plateaux.length-1].num) {
+      plateaux[plateaux.length-1].i.push(i);
+    } else { plateaux.push({num, i: [i]});
+    }}
+    else { plateaux.push({num, i: [i]});
     }
-    if (arr[i+1] === num || arr[i-1] === num){
-      plateau.push({num, i});
-      if (plateau[0].num > arr[plateau[0].i - 1] && plateau[0].num > arr[plateau[plateau.length -1].i + 1]) {
-        pos.push(plateau[0].i);
-        peaks.push(plateau[0].num);
-      }
-    }
-  })
-  return { peaks, pos }
+   } 
+  });
+  console.log(plateaux)
+return { peaks, pos}
 }
+pickPeaks([3,2,3,6,4,1,2,3,2,1,2,2,2,1]);
+pickPeaks([1,2,5,4,3,2,3,6,4,1,2,3,3,4,5,3,2,1,2,3,5,5,4,3])
