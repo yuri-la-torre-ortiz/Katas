3bf927ed231d25682115a2ea96738ffeff1fb58a
Yuri La Torre Ortiz <yuri.la.torre.ortiz@protonmail.ch>
2020-09-03 10:44:10 +0200

    Adjust instruction lines

--- 5kyu/CountIPAddresses.js
+++ 5kyu/CountIPAddresses.js
@@ -1,6 +1,8 @@
-/*Implement a function that receives two IPv4 addresses, and returns the number of addresses between them (including the first one, excluding the last one).
+/*Implement a function that receives two IPv4 addresses, and returns the number of addresses between them 
+(including the first one, excluding the last one).
 
-All inputs will be valid IPv4 addresses in the form of strings. The last address will always be greater than the first one.
+All inputs will be valid IPv4 addresses in the form of strings. 
+The last address will always be greater than the first one.
 Examples
 
 ipsBetween("10.g.0.0", "10.0.0.50")  ===   50 
@@ -11,3 +13,4 @@ ipsBetween("20.0.0.10", "20.0.1.0")  ===  246
 function ipsBetween(start, end){
   //TODO
 }
+
