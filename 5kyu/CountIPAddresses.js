b7d5ac2f0e55aae66ae9a01c6f80ab7bd1291ef7
Yuri La Torre Ortiz <yuri.la.torre.ortiz@protonmail.ch>
2020-09-03 11:08:51 +0200

    Fix typo in examples given

--- 5kyu/CountIPAddresses.js
+++ 5kyu/CountIPAddresses.js
@@ -5,7 +5,7 @@ All inputs will be valid IPv4 addresses in the form of strings.
 The last address will always be greater than the first one.
 Examples
 
-ipsBetween("10.g.0.0", "10.0.0.50")  ===   50 
+ipsBetween("10.0.0.0", "10.0.0.50")  ===   50 
 ipsBetween("10.0.0.0", "10.0.1.0")   ===  256 
 ipsBetween("20.0.0.10", "20.0.1.0")  ===  246
 */
